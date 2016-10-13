7296eb81ecc9f4eaa5acd1dd7565253cbdb7fd93
Leroy Shirto <leroy.shirto@fasthosts.com>
2016-10-13 09:04:34 +0100

    fix messaging
    now returns the node version

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// server.js
+// app.js
 
 // BASE SETUP
 // =============================================================================
@@ -21,10 +21,9 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'hooray! welcome to a Nodejs ' + process.version + ' api!' });
 });
 
-// more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
@@ -33,4 +32,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('api is available on http://localhost:' + port +'/api');
